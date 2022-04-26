1658f20fed6d8ff2328c1a544daf3dd700d8a9ee
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-04-26 19:57:41 +0530

    almost

---
+++ src/components/Register/Homepage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Homepage.css";
+const Homepage = () => {
+  return (
+    <>
+    <div className='cont'>
+
+    <div className='image_left'>
+
+    </div>
+    <div className='image_right'>
+      <div className='text'>
+        <h1>
+          Hooray!
+        </h1>
+
+      </div>
+      <div className='image3'>
+        <h2 className='heading'>
+          <br></br>
+          You've Registered Successfully.
+        </h2>
+      </div>  
+        <div className='image4'>
+
+        </div>
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Homepage;
